f5d688b61d272506569eefb1da6a7c5dd1901377
Gabi <gabrysia.czajkowska@wp.pl>
2024-02-19 23:51:11 +0100

    configuration create-todo

--- src/main/java/com/todolist/controller/TodoCreateController.java
+++ src/main/java/com/todolist/controller/TodoCreateController.java
@@ -1,9 +1,14 @@
 package com.todolist.controller;
 
+import ch.qos.logback.core.model.Model;
+import com.todolist.model.TodoList;
 import com.todolist.service.TodoListService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoCreateController {
@@ -11,7 +16,18 @@ public class TodoCreateController {
     public TodoListService todoListService;
 
     @GetMapping("/create-todo")
-    public String showCreateForm() {
-        return "new-todo-list";
+    public String showCreateForm(TodoList todoList) {
+        return "todo-createpage";
+    }
+
+    @PostMapping("/todo")
+    public String createTodoList(@Valid TodoList todoList, BindingResult result, Model model) {
+
+        TodoList list = new TodoList();
+        list.setDescription(todoList.getDescription());
+        list.setIsComplete(todoList.getIsComplete());
+
+        todoListService.save((todoList));
+        return "redirect:/";
     }
 }
--- src/main/java/com/todolist/model/TodoList.java
+++ src/main/java/com/todolist/model/TodoList.java
@@ -10,7 +10,7 @@ import java.time.Instant;
 @Getter
 @Setter
 @Entity
-@Table(name = "todo_list")
+@Table(name = "todo_lists")
 public class TodoList implements Serializable {
 
     @Id
